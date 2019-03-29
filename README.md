59b27372a555b9cb26e6aaa58df763d60af3b887
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2019-03-29 09:56:56 -0400

    Clean up and add readme

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # binwalk-deltas
 Compute deltas from a binwalk output file for estimating size of objects
 identified by binwalk.
+
+*Note: probably a more useful approach is to just dump the files out, see
+https://github.com/ReFirmLabs/binwalk/wiki/quick-start-guide#file-extraction*
+
+## Usage
+```bash
+# normal binwalk
+➜ binwalk dist/binwalk_deltas-0.1.0-py2.py3-none-any.whl
+
+DECIMAL       HEXADECIMAL     DESCRIPTION
+--------------------------------------------------------------------------------
+0             0x0             Zip archive data, at least v2.0 to extract, compressed size: 803, uncompressed size: 2193, name: binwalk_deltas.py
+850           0x352           Zip archive data, at least v2.0 to extract, compressed size: 406, uncompressed size: 775, name: binwalk_deltas-0.1.0.dist-info/METADATA
+1325          0x52D           Zip archive data, at least v2.0 to extract, compressed size: 95, uncompressed size: 110, name: binwalk_deltas-0.1.0.dist-info/WHEEL
+1486          0x5CE           Zip archive data, at least v2.0 to extract, compressed size: 48, uncompressed size: 56, name: binwalk_deltas-0.1.0.dist-info/entry_points.txt
+1611          0x64B           Zip archive data, at least v2.0 to extract, compressed size: 17, uncompressed size: 15, name: binwalk_deltas-0.1.0.dist-info/top_level.txt
+1702          0x6A6           Zip archive data, at least v2.0 to extract, compressed size: 305, uncompressed size: 502, name: binwalk_deltas-0.1.0.dist-info/RECORD
+2570          0xA0A           End of Zip archive, footer length: 22
+
+# binwalk with deltas
+➜ binwalk-deltas <(binwalk dist/binwalk_deltas-0.1.0-py2.py3-none-any.whl ) dist/binwalk_deltas-0.1.0-py2.py3-none-any.whl
+Offset       Est. Size       Object
+0x0          850 Bytes       Zip archive data, at least v2.0 to extract, compressed size: 803, uncompressed size: 2193, name: binwalk_deltas.py
+0x352        475 Bytes       Zip archive data, at least v2.0 to extract, compressed size: 406, uncompressed size: 775, name: binwalk_deltas-0.1.0.dist-info/METADATA
+0x52d        161 Bytes       Zip archive data, at least v2.0 to extract, compressed size: 95, uncompressed size: 110, name: binwalk_deltas-0.1.0.dist-info/WHEEL
+0x5ce        125 Bytes       Zip archive data, at least v2.0 to extract, compressed size: 48, uncompressed size: 56, name: binwalk_deltas-0.1.0.dist-info/entry_points.txt
+0x64b        91 Bytes        Zip archive data, at least v2.0 to extract, compressed size: 17, uncompressed size: 15, name: binwalk_deltas-0.1.0.dist-info/top_level.txt
+0x6a6        868 Bytes       Zip archive data, at least v2.0 to extract, compressed size: 305, uncompressed size: 502, name: binwalk_deltas-0.1.0.dist-info/RECORD
+0xa0a        22 Bytes        End of Zip archive, footer length: 22
--- binwalk_deltas.py
+++ binwalk_deltas.py
@@ -2,21 +2,31 @@
 Print deltas in binwalk output. Use like:
 python binwalk-deltas.py <(binwalk BINFILE) BINFILE
 """
-
+from __future__ import print_function
 from errno import EPIPE
 import re
 import sys
 import os
 from functools import wraps
-from sys import exit, stderr, stdout
-from traceback import print_exc
 
 import humanize
 
 
-def suppress_broken_pipe_msg(f):
-    @wraps(f)
+def suppress_broken_pipe_msg(function):
+    """Python when printing to a pager (less), if you exit without
+    reading all the output, you get an ugly but harmless exception:
+        BrokenPipeError: [Errno 32] Broken pipe
+    If you try to just trap the error, you still get this print
+    on python3:
+        Exception ignored in: <_io.TextIOWrapper name='<stdout>' mode='w' encoding='UTF-8'>
+        BrokenPipeError: [Errno 32] Broken pipe
+    Found an answer on stackoverflow that gets us close.
+    https://stackoverflow.com/a/35761190
+"""
+
+    @wraps(function)
     def wrapper(*args, **kwargs):
+        """Wrap a function so we don't get ugly BrokenPipeErrors"""
 
         try:
             broken_pipe_exception = BrokenPipeError
@@ -24,39 +34,27 @@ def suppress_broken_pipe_msg(f):
             broken_pipe_exception = IOError
 
         try:
-            return f(*args, **kwargs)
+            return function(*args, **kwargs)
         except SystemExit:
             raise
         except broken_pipe_exception as exc:
             if broken_pipe_exception == IOError:
                 if exc.errno != EPIPE:
                     raise
-        except:
-            print_exc()
-            exit(1)
-        finally:
-            try:
-                stdout.flush()
-            finally:
-                try:
-                    stdout.close()
-                finally:
-                    try:
-                        stderr.flush()
-                    finally:
-                        stderr.close()
 
     return wrapper
 
 
 def print_entry(offset, size, name, human=False):
+    """Print a binwalk entry"""
     if human:
         size = humanize.naturalsize(size)
-    print("0x{:<10x} {:<10} {}".format(offset, size, name))
+    print("0x{:<10x} {:<15} {}".format(offset, size, name))
 
 
 @suppress_broken_pipe_msg
 def main():
+    """Main cli entrance point"""
     binwalk_re = re.compile(r"(\d+)\s+0x[0-9a-fA-F]+\s+(.*)\s*$")
 
     entries = []
@@ -64,19 +62,19 @@ def main():
     filesize = os.stat(sys.argv[2]).st_size
 
     with open(sys.argv[1], "r") as infile:
-        lastmatch = None
+        lastmatch = (None, None)
         for line in infile.readlines():
-            m = binwalk_re.match(line.strip())
-            if m:
-                newmatch = (int(m.group(1)), m.group(2))
-                if lastmatch:
+            match = binwalk_re.match(line.strip())
+            if match:
+                newmatch = (int(match.group(1)), match.group(2))
+                if lastmatch != (None, None):
                     entries.append(
                         (lastmatch[0], newmatch[0] - lastmatch[0], lastmatch[1])
                     )
                 lastmatch = tuple(newmatch)
         entries.append((lastmatch[0], filesize - lastmatch[0], lastmatch[1]))
 
-    print("{:<12} {:<10} {}".format("Offset", "Est. Size", "Object"))
+    print("{:<12} {:<15} {}".format("Offset", "Est. Size", "Object"))
     for offset, size, name in entries:
         print_entry(offset, size, name, True)
 
